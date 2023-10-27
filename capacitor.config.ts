01d8feb10aec94ee319f3869cdaa82c97d70827b
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2023-10-27 19:32:43 -0300

    chore: Sets up MKCloud info on config file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,21 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter.testnnclsecond',
   appName: 'test-notifications-salesforce-mkcloud',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  cordova: {
+    preferences: {
+      "com.salesforce.marketingcloud.app_id":"",
+      "com.salesforce.marketingcloud.access_token":"",
+      "com.salesforce.marketingcloud.tenant_specific_endpoint":"",
+      "com.salesforce.marketingcloud.analytics": "false",
+      "salesforce.marketingcloud.delay_registration_until_contact_key_is_set": "false",
+      "com.salesforce.marketingcloud.notification_small_icon": "ic_launcher_foreground"        
+    }
   }
 };
 
